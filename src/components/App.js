d5c1d0b4999eeec142b9861605787bc3f0e37e27
Balaji Anoop Gupta <balajianoopgupta.ba@gmail.com>
2018-03-14 01:29:52 -0700

    Installed react-icons and displayed mealTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
-import {addRecipe, removeFromCalendar} from '../actions'
+import {connect} from 'react-redux';
+import {addRecipe, removeFromCalendar} from '../actions';
+import {capitalize} from '../utils/helpers';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
+
 class App extends Component {
 
   doThing = () => {
@@ -9,10 +12,18 @@ class App extends Component {
   }
 
   render() {
-    console.log('Props', this.props)
+    const {calendar, remove} = this.props;
+    const mealOrder = ['breakfast','lunch','dinner'];
+
     return (
-      <div>
-        Hello World
+      <div className='container'>
+        <ul className='meal-types'>
+          {mealOrder.map((mealType) => (
+            <li key={mealType} className='subheader'>
+              {capitalize(mealType)}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
